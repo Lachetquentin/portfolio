191ffa525b3c50f3b3f686908c7d07157efe9d93
Lachetquentin <lachet.quentin@protonmail.com>
2022-08-30 16:09:25 +0200

    Refacto image importAll + Add Docker

--- src/data/Skills.js
+++ src/data/Skills.js
@@ -1,77 +1,85 @@
 function importAll(r) {
-    return r.keys().map(r);
+    var images = {};
+    r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+        return images;
 }
 const images = importAll(require.context('../assets/Technologies', false, /\.(png|jpe?g)$/));
 
 export const Technologies = [{
     id: 1,
     name: "HTML",
-    image: images[5],
+    image: images['html.png'],
     size: "w-20"
 },
 {
     id: 2,
     name: "CSS",
-    image: images[2],
+    image: images['css.png'],
     size: "w-20"
 },
 {
     id: 3,
     name: "Javascript",
-    image: images[7],
+    image: images['javascript.png'],
     size: "w-20"
 },
 {
     id: 4,
     name: "PHP",
-    image: images[11],
+    image: images['php.png'],
     size: "h-20"
 },
 {
     id: 5,
     name: "React",
-    image: images[12],
+    image: images['react.png'],
     size: "w-20"
 },
 {
     id: 6,
     name: "Node.js",
-    image: images[10],
+    image: images['nodejs.png'],
     size: "w-20"
 },
 {
     id: 7,
     name: "Git",
-    image: images[3],
+    image: images['git.png'],
     size: "w-20"
 },
 {
     id: 8,
     name: "Tailwind",
-    image: images[13],
+    image: images['tailwind.png'],
     size: "w-20"
 },
 {
     id: 9,
     name: "Bootstrap",
-    image: images[0],
+    image: images['bootstrap.png'],
     size: "w-20"
 },
 {
     id: 10,
     name: "MySQL",
-    image: images[9],
+    image: images['mysql.png'],
     size: "w-20"
 },
 {
     id: 11,
     name: "C#",
-    image: images[1],
+    image: images['csharp.png'],
     size: "w-20"
 },
 {
     id: 12,
     name: "Java",
-    image: images[6],
+    image: images['java.png'],
     size: "h-20"
-}]
+},
+{
+    id: 13,
+    name: "Docker",
+    image: images['docker.png'],
+    size: "h-20"
+}];
